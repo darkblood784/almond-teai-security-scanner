06bb851f87ba9da0c9023f958e01588af0282742
ADARSH ROUNIYAR <58559578+darkblood784@users.noreply.github.com>
2026-04-20 18:13:13 +0800

    Update security-test.ts

--- security-test.ts
+++ security-test.ts
@@ -1,11 +1,2 @@
---- a/security-test.ts
-+++ b/security-test.ts
-@@ -4,4 +4,4 @@
- // Security test file
- 
--import { config } from 'dotenv';
--config();
-+const token = process.env.SECRET_TOKEN;
-+if (!token) { throw new Error('SECRET_TOKEN environment variable is required'); }
--export const getToken = () => token;
-+export const getToken = () => token;
+const token = "dsflslkhdsgsdgsdsgs43546546132134fsdjfhns";
+const usdt = "5465321546763543213213213213546587467654654";
